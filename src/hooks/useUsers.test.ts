68170192e15f966e326b2744a0ed191c895333f6
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-10 15:14:50 -0600

    Update useUsers.test.ts
    fix testcase to updates only the relevant user

--- src/hooks/useUsers.test.ts
+++ src/hooks/useUsers.test.ts
@@ -26,6 +26,40 @@ describe("useUsers", () => {
   });
 
   it("updates only the relevant user called by updateUser", () => {
-    expect(null).toStrictEqual([{ idUser: 0 }]);
+    const idUserToEdit = 1001;
+    const { result } = renderHook(() => useUsers());
+    act(() => {
+      jest.advanceTimersByTime(TIME_DELAY + 1);
+    });
+    const usersBeforeUpdateOnlyUser = result.current.data;
+    const beforeUsersMap = new Map(
+      usersBeforeUpdateOnlyUser?.map((user) => [user.idUser, user])
+    );
+    expect(beforeUsersMap.has(idUserToEdit)).toBe(true);
+    const newUserData = {
+        idUser: idUserToEdit,
+        profile: {
+        firstName: "Adam",
+        lastName: "Alpaca",
+      },
+      role: "Director",
+    };
+    act(() => {
+      result.current.updateUser(idUserToEdit, newUserData);
+    });
+    act(() => {
+      jest.advanceTimersByTime(TIME_DELAY + 1);
+    });
+    const usersAfterUpdateOnlyUser = result.current.data;
+    usersAfterUpdateOnlyUser?.forEach((user) => {
+      if(user.idUser === idUserToEdit) {
+        expect(user).toStrictEqual(newUserData);
+      } else {
+        expect(user).toBe(
+          beforeUsersMap.get(user.idUser)
+        );
+      }
+    })
+    expect(usersAfterUpdateOnlyUser).toHaveLength(usersBeforeUpdateOnlyUser?.length ?? 0);
   });
 });
