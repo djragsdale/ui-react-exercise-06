eee56c738c5e87accba63e9eabcaa4263a6bd13e
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-10 15:14:06 -0600

    Update App.tsx
    show alert oneditinguser

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,38 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./App.css";
 
-import { Card, Classes } from "@blueprintjs/core";
+import { Card, Classes, Alert } from "@blueprintjs/core";
 import { UserList } from "./components/UserList";
+
 import { useUsers } from "./hooks/useUsers";
+import { useAlert } from "./hooks/useAlert";
+
+import type { User } from "./types/User";
 
 const App = () => {
-  const { data, isLoading } = useUsers();
+  const { data, isLoading, updateUser } = useUsers();
+  const { messageAlert, isOpenAlert, showSuccess, closeAlert } = useAlert();
+
+  const handleOnEditUser = (idUser: number, user: User) => {
+    updateUser(idUser, user);
+    showSuccess();
+  }
 
   return (
     <div className={`App ${Classes.DARK}`}>
       <h1>UI Screening 01</h1>
       <Card>
-        <UserList isLoading={isLoading} users={data} />
+        <UserList isLoading={isLoading} users={data} onEditUser={(userItem) => handleOnEditUser(userItem.idUser, userItem)} />
       </Card>
+      <Alert
+        confirmButtonText="Okay"
+        intent="success"
+        isOpen={isOpenAlert}
+        loading={isLoading}
+        onClose={closeAlert}
+      >
+        <p>{messageAlert}</p>
+      </Alert>
     </div>
   );
 };
