29b0bad1a73cc8b45a5bf198ffec97aee4f7447a
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-10 15:12:49 -0600

    Create useAlert.ts
    add alert for success

---
+++ src/hooks/useAlert.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const useAlert = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const closeAlert = () => {
+    setIsOpen(false);
+  }
+
+  const showAlert = (message: string) => {
+    setMessage(message);
+    setIsOpen(true);
+  }
+
+  const showSuccess = (message = "User updated successfully!") => {
+    showAlert(message);
+  }
+
+  return {
+    isOpenAlert: isOpen,
+    messageAlert: message,
+    closeAlert,
+    showAlert,
+    showSuccess,
+  }
+};
