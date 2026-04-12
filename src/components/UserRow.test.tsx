ba8dd2ed77c9cc5ca71e3bb57f73c9ea185cfd62
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-12 02:01:50 -0600

    Create UserRow.test.tsx

---
+++ src/components/UserRow.test.tsx
@@ -0,0 +1,38 @@
+import { act, render } from "@testing-library/react";
+
+import { UserRow } from "./UserRow";
+
+const testUser = {
+    idUser: 1,
+    profile: {
+      firstName: "ABC",
+      lastName: "DEF",
+    },
+    role: "Role1",
+};
+
+describe("UserRow", () => {
+  it("renders a user in a row", () => {
+    const { container } = render(<table><tbody><UserRow userItem={testUser} /></tbody></table>);
+    const row = container.querySelector("tr");
+    expect(row?.textContent).toMatch("1");
+    expect(row?.textContent).toMatch("ABC");
+    expect(row?.textContent).toMatch("DEF");
+    expect(row?.textContent).toMatch("Role1");
+  });
+
+  it("invokes onEditUser callback when Edit button is pressed", () => {
+    const onEditUser = jest.fn();
+    const { container } = render(<table><tbody>
+                                          <UserRow userItem={testUser} onEditUser={onEditUser} />
+                                        </tbody>
+                                </table>);
+    const row = container.querySelector("tr");
+    const editButton = row?.querySelector<"button">("button");
+    expect(editButton).toBeTruthy();
+    act(() => {
+        editButton?.click();
+    });
+    expect(onEditUser).toHaveBeenLastCalledWith(testUser);
+  });
+});
