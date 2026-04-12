357bf989a91997f12b62cbad68ae9a26d7fe50f4
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-12 02:01:03 -0600

    Create UserRow.tsx
    update userlistitem  and rename to userrow

---
+++ src/components/UserRow.tsx
@@ -0,0 +1,26 @@
+import { MouseEvent } from "react";
+import type { User } from "../types/User";
+
+import { Button } from "@blueprintjs/core";
+
+type UserRowProps = {
+  onEditUser?: (user: User) => void;
+  userItem: User;
+};
+
+export const UserRow = ({ userItem, onEditUser }: UserRowProps) => {
+    const onEditUserRow = (event: MouseEvent<HTMLElement>) => {
+        onEditUser?.(userItem);
+        event.currentTarget.blur();
+    }
+
+    return <tr>
+        <td>{userItem.idUser}</td>
+        <td>{userItem.profile.firstName}</td>
+        <td>{userItem.profile.lastName}</td>
+        <td>{userItem.role}</td>
+        <td>
+            <Button icon="edit" aria-label="edit" intent="primary" onClick={onEditUserRow} />
+        </td>
+    </tr>
+};
