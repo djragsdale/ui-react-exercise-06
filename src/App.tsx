c9f27cbb64f94220823cd9faa7c164081cfe629f
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-12 02:07:57 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "./App.css";
 
-import { Card, Classes, Alert } from "@blueprintjs/core";
+import { Card, Classes } from "@blueprintjs/core";
 import { UserList } from "./components/UserList";
 import { EditUserDialog } from "./components/EditUserDialog";
 
 import { useUsers } from "./hooks/useUsers";
-import { useAlert } from "./hooks/useAlert";
 import { useEditUserDialog } from "./hooks/useEditUserDialog";
 
 import type { User } from "./types/User";
 
 const App = () => {
   const { data, isLoading, updateUser } = useUsers();
-  const { messageAlert, isOpenAlert, showSuccess, closeAlert } = useAlert();
   const {
     isOpenEditUserDialog,
     selectedUserEditUserDialog,
@@ -22,11 +20,10 @@ const App = () => {
     closeEditUserDialog,
   } = useEditUserDialog();
 
-  const handleOnEditUser = (user: User | null) => {
+  const handleOnEditUser = (user?: User) => {
     if(!user) return;
     updateUser(user.idUser, user);
     closeEditUserDialog();
-    showSuccess();
   }
 
   return (
@@ -36,20 +33,11 @@ const App = () => {
         <UserList isLoading={isLoading} users={data} onEditUser={showEditUserDialog} />
       </Card>
       <EditUserDialog
-        onEditUser={() => handleOnEditUser(selectedUserEditUserDialog)}
-        onClose={closeEditUserDialog}
         isOpen={isOpenEditUserDialog}
-        messageText={"example text"}
+        userData={selectedUserEditUserDialog}
+        onEditUser={handleOnEditUser}
+        onClose={closeEditUserDialog}
       />
-      <Alert
-        confirmButtonText="Ok"
-        intent="success"
-        isOpen={isOpenAlert}
-        loading={isLoading}
-        onClose={closeAlert}
-      >
-        <p>{messageAlert}</p>
-      </Alert>
     </div>
   );
 };
