72d5db482b0302801466caf62291297f4c0bdae0
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-12 03:21:27 -0600

    Create TextSelect.test.tsx
    add text select test

---
+++ src/components/ui/TextSelect.test.tsx
@@ -0,0 +1,60 @@
+import { act, render, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { TextSelect } from "./TextSelect";
+import { Button } from "@blueprintjs/core";
+
+const testRoles = ["Enginner", "Administrator", "Support Technician"];
+
+describe("TextSelect", () => {
+  it("renders text select", () => {
+    render(
+    <TextSelect
+      items={testRoles}
+      usePortal={false}
+      onItemSelect={jest.fn()}
+      trigger={<Button text="Select role" />}
+    />
+    );
+
+    expect(screen.getByText("Select role")).toBeInTheDocument();
+   });
+
+  it("text select is open when click on trigger", async () => {
+    const user = userEvent.setup();
+
+    render(
+        <TextSelect
+        items={testRoles}
+        usePortal={false}
+        onItemSelect={jest.fn()}
+        trigger={<Button text="Select role" />}
+        />
+    );
+
+    expect(screen.getByText("Select role")).toBeInTheDocument();
+
+    await user.click(screen.getByText("Select role"));
+
+    const popover = await screen.queryByRole("listbox");
+
+    expect(popover).toBeInTheDocument();
+  });
+
+  it("invokes onItemSelect callback when menuitem is pressed", async () => {
+    const onItemSelect = jest.fn();
+    const user = userEvent.setup();
+    render(<TextSelect
+        items={testRoles}
+        usePortal={false}
+        onItemSelect={onItemSelect}
+        trigger={<Button text="Select role" />}
+    />);
+
+    await user.click(screen.getByText("Select role"));
+    await user.click(await screen.findByText(testRoles[0]));
+
+    expect(onItemSelect).toHaveBeenCalled();
+    expect(onItemSelect.mock.calls[0][0]).toBe(testRoles[0]);
+  });
+});
