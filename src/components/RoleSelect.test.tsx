6999ccd8df0c6b4e2c3db506d9897387cb7163db
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-13 05:36:17 -0600

    Update RoleSelect.test.tsx

--- src/components/RoleSelect.test.tsx
+++ src/components/RoleSelect.test.tsx
@@ -1,46 +1,42 @@
-import { useState } from "react";
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import { roles, type Role } from "../types/Role";
+import type { Role } from "../types/Role";
 
 import { RoleSelect } from "./RoleSelect";
 
+const NEW_ROLE: Role = "Administrator";
+
+const renderRoleSelect = (props = {}) => {
+  const selectedRole: Role = "Engineer";
+  const handlers = {
+      onChangeRole: jest.fn(),
+  };
+  const testingUtils = render(<RoleSelect
+                                role={selectedRole}
+                                {...props}
+                                {...handlers}
+                              />);
+  return {
+    user: userEvent.setup(),
+    selectedRole,
+    handlers,
+    ...testingUtils,
+  };
+};
+
 describe("RoleSelect", () => {
   it("renders an html select for select role", () => {
-    render(
-    <RoleSelect role={roles[0]} onChangeRole={jest.fn()}/>
-    );
-
-    expect(screen.getByRole("combobox")).toBeInTheDocument();
-    expect(screen.getByRole("combobox")).toHaveValue(roles[0]);
+    const { getByRole, selectedRole } = renderRoleSelect();
+    expect(getByRole("combobox")).toBeInTheDocument();
+    expect(getByRole("combobox")).toHaveValue(selectedRole);
    });
 
-  it("calls onChangeRole and updates the selected value", async () => {
-    const user = userEvent.setup();
-    const onChangeRole = jest.fn();
-
-    const Container = () => {
-      const [role, setRole] = useState<Role>(roles[0]);
-
-      const handleChange = (r: Role) => {
-        onChangeRole(r);
-        setRole(r);
-      };
-
-      return (
-        <RoleSelect
-          role={role}
-          onChangeRole={handleChange}
-        />
-      )
-    };
-
-    render(<Container />);
-    const select = screen.getByRole("combobox");
-    await user.selectOptions(select, roles[1]);
-    expect(onChangeRole).toHaveBeenCalledTimes(1);
-    expect(onChangeRole).toHaveBeenCalledWith(roles[1]);
-    expect(select).toHaveValue(roles[1]);
+  it.only("calls onChangeRole and updates the selected value", async () => {
+    const { user, getByRole, handlers } = renderRoleSelect();
+    const select = getByRole("combobox");
+    await user.selectOptions(select, NEW_ROLE);
+    expect(handlers.onChangeRole).toHaveBeenCalledTimes(1);
+    expect(handlers.onChangeRole).toHaveBeenCalledWith(NEW_ROLE);
   });
 });
