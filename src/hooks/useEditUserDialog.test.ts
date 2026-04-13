d5535282aead30990230c8aedea7fbbb5d226864
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-13 05:35:40 -0600

    Create useEditUserDialog.test.ts

---
+++ src/hooks/useEditUserDialog.test.ts
@@ -0,0 +1,37 @@
+import { renderHook, act } from "@testing-library/react";
+import { useEditUserDialog } from "./useEditUserDialog";
+
+import type { Role } from "../types/Role";
+
+const OLD_ROLE: Role = "Engineer";
+const createTestUser = () => ({
+    idUser: 1,
+    profile: {
+      firstName: "ABC",
+      lastName: "DEF",
+    },
+    role: OLD_ROLE,
+});
+
+const testUser = createTestUser();
+
+describe("useEditUserDialog", () => {
+    it("opens dialog and sets selected user", () => {
+        const { result } = renderHook(() => useEditUserDialog());
+        act(() => {
+            result.current.showEditUserDialog(testUser);
+        });
+        expect(result.current.isOpenEditUserDialog).toBe(true);
+        expect(result.current.selectedUserEditUserDialog).toEqual(testUser);
+    });
+
+    it("closes dialog and clears selected user", () => {
+        const { result } = renderHook(() => useEditUserDialog());
+        act(() => {
+            result.current.showEditUserDialog(testUser);
+            result.current.closeEditUserDialog();
+        });
+        expect(result.current.isOpenEditUserDialog).toBe(false);
+        expect(result.current.selectedUserEditUserDialog).toBeUndefined();
+    });
+});
