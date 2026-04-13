648b9394059357aff7f48d4409aa2f3cf706ce0c
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-13 05:40:39 -0600

    Update UserRow.test.tsx
    improve user row

--- src/components/UserRow.test.tsx
+++ src/components/UserRow.test.tsx
@@ -1,39 +1,51 @@
-import { act, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { type Role } from "../types/Role";
 
 import { UserRow } from "./UserRow";
-import { roles } from "../types/Role";
+import { User } from "../types/User";
 
-const testUser = {
+const OLD_ROLE: Role = "Engineer";
+
+const createTestUser = () => ({
     idUser: 1,
     profile: {
       firstName: "ABC",
       lastName: "DEF",
     },
-    role: roles[0],
+    role: OLD_ROLE,
+});
+
+const renderRow = (props = {}) => {
+  const testUser: User = createTestUser();
+  const callbacks = { onEditUser: jest.fn() };
+  const container = <table>
+                      <tbody>
+                        <UserRow userItem={testUser} {...props} {...callbacks} />
+                      </tbody>
+                    </table>;
+  return ({
+    user: userEvent.setup(),
+    rowUser: testUser,
+    callbacks,
+    ...render(container)
+  });
 };
 
 describe("UserRow", () => {
   it("renders a user in a row", () => {
-    const { container } = render(<table><tbody><UserRow userItem={testUser} /></tbody></table>);
+    const { container, rowUser } = renderRow();
     const row = container.querySelector("tr");
-    expect(row?.textContent).toMatch("1");
-    expect(row?.textContent).toMatch("ABC");
-    expect(row?.textContent).toMatch("DEF");
-    expect(row?.textContent).toMatch(roles[0]);
+    expect(row?.textContent).toMatch(rowUser.idUser.toString());
+    expect(row?.textContent).toMatch(rowUser.profile.firstName);
+    expect(row?.textContent).toMatch(rowUser.profile.lastName);
+    expect(row?.textContent).toMatch(rowUser.role);
   });
 
-  it("invokes onEditUser callback when Edit button is pressed", () => {
-    const onEditUser = jest.fn();
-    const { container } = render(<table><tbody>
-                                          <UserRow userItem={testUser} onEditUser={onEditUser} />
-                                        </tbody>
-                                </table>);
-    const row = container.querySelector("tr");
-    const editButton = row?.querySelector<"button">("button");
-    expect(editButton).toBeTruthy();
-    act(() => {
-        editButton?.click();
-    });
-    expect(onEditUser).toHaveBeenLastCalledWith(testUser);
+  it("invokes onEditUser callback when Edit button is pressed", async () => {
+    const { user, getByRole, rowUser, callbacks } = renderRow();
+    await user.click(getByRole("button", { name: /edit/i }));
+    expect(callbacks.onEditUser).toHaveBeenLastCalledWith(rowUser);
   });
 });
