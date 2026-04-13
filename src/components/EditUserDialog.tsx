31afb135a01b27ab268d6165d73b818689b227a1
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-12 23:14:30 -0600

    Update EditUserDialog.tsx

--- src/components/EditUserDialog.tsx
+++ src/components/EditUserDialog.tsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react";
-
-import type { User } from "../types/User";
-import type { Role } from "../types/Role";
-
-import { UIDialog } from "./ui/UIDialog";
 import { Card, Button } from "@blueprintjs/core";
 
+import type { User } from "../types/User";
+import { type Role, roles } from "../types/Role";
+
+import { UIDialog } from "./ui/UIDialog";
 import { RoleSelect } from "./RoleSelect";
 
+import "./EditUserDialog.scss";
+
 interface EditUserDialog {
-    onEditUser?: (idUser: number, editedUserData: User) => void;
-    isOpen?: boolean;
+    onUpdateUser: (idUser: number, editedUserData: User) => void;
+    isOpen: boolean;
     userData?: User;
-    onClose?: () => void;
+    onClose: () => void;
 }
 
-export const EditUserDialog = ({ isOpen, userData, onEditUser, onClose, }: EditUserDialog) => {
-    const [editedUserData, setEditedUserData] = useState<User | undefined>(userData);
+export const EditUserDialog = ({ isOpen, userData, onUpdateUser, onClose, }: EditUserDialog) => {
+    const [editedUserData, setEditedUserData] = useState<User|undefined>(userData);
 
     useEffect(() => {
         setEditedUserData(userData);
@@ -25,16 +26,17 @@ export const EditUserDialog = ({ isOpen, userData, onEditUser, onClose, }: EditU
     const handleOnChangeRole = (role: Role) => {
         setEditedUserData(prevUser => {
             if(!prevUser) return prevUser;
-            
             return {...prevUser, role, }
         });
     }
 
-    const handleEditUser = (user?: User) => {
-        if(!user) return;
-        onEditUser?.(user.idUser, user);
+    const handleEditUser = () => {
+        if(!editedUserData) return;
+        onUpdateUser?.(editedUserData.idUser, editedUserData);
         onClose?.();
     }
+
+    if (!editedUserData) return null;
 
     return <UIDialog
             title="Edit User"
@@ -43,36 +45,30 @@ export const EditUserDialog = ({ isOpen, userData, onEditUser, onClose, }: EditU
             icon="edit"
             footerActions={(<>
                               <Button onClick={onClose}>Cancel</Button>
-                              <Button onClick={() => handleEditUser?.(editedUserData)}>Save</Button>
+                              <Button onClick={handleEditUser}>Save</Button>
                             </>
                           )}
             >
-            <Card>
-                <table style={{ width: "100%", tableLayout: "fixed" }}>
-                    <tbody>
-                    <tr>
-                        <td>Id: </td>
-                        <td>{userData?.idUser}</td>
-                    </tr>
-                    <tr>
-                        <td>FirstName: </td>
-                        <td>{userData?.profile.firstName}</td>
-                    </tr>
-                    <tr>
-                        <td>LastName: </td>
-                        <td>{userData?.profile.lastName}</td>
-                    </tr>
-                    <tr>
-                        <td style={{ width: "500px" }}>Role: </td>
-                        <td>
-                            <RoleSelect
-                                role={editedUserData?.role}
-                                onChangeRole={handleOnChangeRole}
-                            />
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
+            <Card className="cardUserDialog">
+                <div>
+                    <span className="titleUserDialog"><strong>Id:</strong></span>
+                    <span>{editedUserData.idUser}</span>
+                </div>
+                <div>
+                    <span className="titleUserDialog"><strong>Firstname:</strong></span>
+                    <span>{editedUserData.profile.firstName}</span>
+                </div>
+                <div>
+                    <span className="titleUserDialog"><strong>Lastname:</strong></span>
+                    <span>{editedUserData.profile.lastName}</span>
+                </div>
+                <div>
+                    <span className="titleUserDialog"><strong>Role:</strong></span>
+                    <RoleSelect
+                        role={editedUserData?.role?? roles[0]}
+                        onChangeRole={handleOnChangeRole}
+                    />
+                </div>
             </Card>
           </UIDialog>
 };
