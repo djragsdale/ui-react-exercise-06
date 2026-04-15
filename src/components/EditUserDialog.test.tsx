d05642aceb886c3bfd6a4ea968cd8d8220d6b48d
anardzm53 <107815205+anardzm53@users.noreply.github.com>
2026-04-15 02:35:08 -0600

    Update EditUserDialog.test.tsx

--- src/components/EditUserDialog.test.tsx
+++ src/components/EditUserDialog.test.tsx
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { useState } from "react";
 
 import { EditUserDialog } from "./EditUserDialog";
 import type { User } from "../types/User";
@@ -14,66 +15,103 @@ const createTestUser = () => ({
     },
     role: OLD_ROLE,
 });
-const renderEditUserDialog = (props = {}) => {
-  const userData: User = createTestUser();
-  const handlers = {
-      close: jest.fn(),
-      updateUser: jest.fn()
+
+const renderEditUserDialog = (props : { editedUser?: User | null } = {}) => {
+  const userData: User = props.editedUser ?? createTestUser();
+  const editUserDialogActions = {
+    close: jest.fn(),
+    show: jest.fn()
   };
-  const testingUtils = render(<EditUserDialog
-                                isOpen={true}
-                                userData={userData}
-                                {...props}
-                                {...handlers}
-                              />);
+  const editUserDialogHandlers = {
+    onChangeRole: jest.fn(),
+    saveUser: jest.fn(() => {
+      editUserDialogActions.close();
+    })
+  };
+  const Container = () => {
+    const [editedUserData, setEditedUserData] = useState<User | null>(userData);
+    return (<EditUserDialog
+              isOpen={true}
+              editedUserData={editedUserData}
+              {...props}
+              saveUser={editUserDialogHandlers.saveUser}
+              onChangeRole={(role:string) => {
+                editUserDialogHandlers.onChangeRole(role);
+                setEditedUserData((prevUser: User | null) => {
+                  if(!prevUser) return prevUser;
+                  return {...prevUser, role};
+                });
+              }}
+              close={editUserDialogActions.close}
+          />);
+  }
+  const testingUtils = render(<Container />);
   return {
     user: userEvent.setup(),
-    userData,
-    handlers,
+    editUserDialogActions,
+    editUserDialogHandlers,
     ...testingUtils,
   };
 };
 
 describe("EditUserDialog", () => {
   it("If the user chooses to cancel the role change, the UserList does not update", async () => {
-    const { user, getByRole, handlers } = renderEditUserDialog();
+    const {
+      user,
+      getByRole,
+      editUserDialogActions: { close },
+      editUserDialogHandlers: { saveUser }
+    } = renderEditUserDialog();
     await user.click(getByRole("button", { name: /^cancel$/i }));
-    expect(handlers.updateUser).not.toHaveBeenCalled();
-    expect(handlers.close).toHaveBeenCalledTimes(1);
+    expect(saveUser).not.toHaveBeenCalled();
+    expect(close).toHaveBeenCalledTimes(1);
   });
 
   it("The UserList does not update until the user clicks a Save button in the dialog", async () => {
-    const { user, userData, getByRole, handlers } = renderEditUserDialog();
+    const userData = createTestUser();
+    const {
+      user,
+      getByRole,
+      editUserDialogHandlers: { onChangeRole, saveUser },
+    } = renderEditUserDialog({ editedUser: userData });
     const expectedUser = { ...userData, role: NEW_ROLE };
     const select = getByRole("combobox");
     expect(select).toBeInTheDocument();
-    expect(select).toHaveValue(userData.role);
+    expect(select).toHaveValue(OLD_ROLE);
     await user.selectOptions(select, NEW_ROLE);
+    expect(onChangeRole).toHaveBeenCalledWith(NEW_ROLE);
     expect(select).toHaveValue(NEW_ROLE);
-    expect(handlers.updateUser).not.toHaveBeenCalled();
+    expect(saveUser).not.toHaveBeenCalled();
     await user.click(getByRole("button", { name: /^save$/i }));
-    expect(handlers.updateUser).toHaveBeenCalled();
-    expect(handlers.updateUser).toHaveBeenCalledWith(userData.idUser, expectedUser);
+    expect(saveUser).toHaveBeenCalled();
+    expect(saveUser).toHaveBeenCalledWith(expectedUser);
   });
 
   it("The dialog closes when the user saves the role change", async () => {
-    const { user, userData, getByRole, handlers } = renderEditUserDialog();
+    const {
+      user,
+      getByRole,
+      editUserDialogActions,
+      editUserDialogHandlers: { onChangeRole, saveUser }
+    } = renderEditUserDialog();
     const select = getByRole("combobox");
     expect(select).toBeInTheDocument();
-    expect(select).toHaveValue(userData.role);
+    expect(select).toHaveValue(OLD_ROLE);
     await user.selectOptions(select, NEW_ROLE);
-    expect(select).toHaveValue(NEW_ROLE);
-    expect(handlers.updateUser).not.toHaveBeenCalled();
+    expect(onChangeRole).toHaveBeenCalled();
+    expect(onChangeRole).toHaveBeenCalledWith(NEW_ROLE);
+    expect(saveUser).not.toHaveBeenCalled();
     await user.click(getByRole("button", { name: /^save$/i }));
-    expect(handlers.updateUser).toHaveBeenCalled();
-    expect(handlers.close).toHaveBeenCalled();
+    expect(saveUser).toHaveBeenCalled();
+    expect(editUserDialogActions.close).toHaveBeenCalled();
   });
 
   it("The dialog displays all available user data, even values not being changed", () => {
-    const { userData, getByText } = renderEditUserDialog();
-    expect(getByText(userData.idUser)).toBeInTheDocument();
-    expect(getByText(userData.profile.firstName)).toBeInTheDocument();
-    expect(getByText(userData.profile.lastName)).toBeInTheDocument();
-    expect(getByText(userData.role)).toBeInTheDocument();
+    const userData = createTestUser();
+    const { getByText } = renderEditUserDialog({ editedUser: userData });
+    expect(getByText(userData?.idUser)).toBeInTheDocument();
+    expect(getByText(userData?.profile.firstName)).toBeInTheDocument();
+    expect(getByText(userData?.profile.lastName)).toBeInTheDocument();
+    expect(getByText(userData?.role)).toBeInTheDocument();
     });
 });
